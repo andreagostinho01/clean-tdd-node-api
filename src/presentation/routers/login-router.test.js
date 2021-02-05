513b564297d8b470eae9fcc5efb01f37b2fd53c0
andre <andre.carlos.agostinho@gmail.com>
2021-02-04 22:06:24 -0300

    Should return 400 if an invalid email is provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,5 +1,6 @@
 const LoginRouter = require('./login-router');
 const MissingParamError = require('../helpers/missing-param-error');
+const InvalidParamError = require('../helpers/invalid-param-error');
 const UnauthorizedError = require('../helpers/unauthorized-error');
 const ServerError = require('../helpers/server-error');
 
@@ -26,15 +27,29 @@ const makeAuthUseCaseWithError = () => {
   return new AuthUseCaseSpy();
 };
 
+const makeEmailValidator = () => {
+  class EmailValidatorSpy {
+    async validate(email) {
+      return this.isEmailValid;
+    }
+  }
+
+  const emailValidatorSpy = new EmailValidatorSpy();
+  emailValidatorSpy.isEmailValid = true;
+  return emailValidatorSpy;
+};
+
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase();
+  const emailValidatorSpy = makeEmailValidator();
   authUseCaseSpy.accessToken = 'valid_token';
 
-  const sut = new LoginRouter(authUseCaseSpy);
+  const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy);
 
   return {
     sut,
     authUseCaseSpy,
+    emailValidatorSpy,
   };
 };
 
@@ -74,6 +89,22 @@ describe('Login Router', () => {
       only the values 
     */
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
+  });
+
+  test('Should return 400 if an invalid email is provided', async () => {
+    const { sut, emailValidatorSpy } = makeSut();
+    emailValidatorSpy.isEmailValid = false;
+
+    const httpRequest = {
+      body: {
+        email: 'invalid@email.com',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = await sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
 
   test('Should return 400 if no password is provided', async () => {
