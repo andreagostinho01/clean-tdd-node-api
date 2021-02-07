2a19446cce1d42a916e46ce5475648525ce5f449
andre <andre.carlos.agostinho@gmail.com>
2021-02-07 12:58:19 -0300

    remove console error and add eslint rule to allow console logs

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -27,7 +27,6 @@ module.exports = class LoginRouter {
 
       return HttpResponse.ok({ accessToken });
     } catch (error) {
-      console.error(error);
       return HttpResponse.serverError();
     }
   }
