385953c01c0af0529fd67dda491ca06bc311331d
andre <andre.carlos.agostinho@gmail.com>
2021-02-08 14:33:17 -0300

    move Sut creation to a factory method

--- src/utils/email-validator.test.js
+++ src/utils/email-validator.test.js
@@ -6,9 +6,11 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => new EmailValidator();
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', async () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = await sut.isValid('valid@email.com');
 
     expect(isEmailValid).toBe(true);
@@ -17,7 +19,7 @@ describe('Email Validator', () => {
   test('Should return false if validator returns false', async () => {
     validator.isEmailValid = false;
 
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = await sut.isValid('invalid@email.com');
 
     expect(isEmailValid).toBe(false);
